113bf1e4a90f2d5eedfa94a57649d9ede2ec6c2e
cmong0516 <cmong0516@gmail.com>
2023-06-29 10:23:24 +0900

    2023.06.29 first

---
+++ src/main/java/com/hello/foreverpet/ForeverpetApplication.java
@@ -0,0 +1,13 @@
+package com.hello.foreverpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeverpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeverpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/foreverpet/ForeverpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.foreverpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeverpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
