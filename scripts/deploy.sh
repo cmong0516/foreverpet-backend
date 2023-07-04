0c3c5e182bd684dbe8e6d3ccb8b3253cea7a7387
cmong0516 <cmong0516@gmail.com>
2023-07-05 00:32:41 +0900

    ci(CI Settings): CI Settings
    CI Settings
    - EC2 폴더에 직접 생성한 application.yml 등록

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application.yml\
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
