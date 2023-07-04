b95eab3d4bc1b9cdf27d01e8af1f47153ce772c7
cmong0516 <cmong0516@gmail.com>
2023-07-04 14:27:44 +0900

    ci(CI Settings): CI Settings
    CI Settings
    - Add travis.yml , Add appspec.yml

--- src/test/java/com/hello/foreverpet/ForeverpetApplicationTests.java
+++ src/test/java/com/hello/foreverpet/ForeverpetApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hello.foreverpet;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ForeverpetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
