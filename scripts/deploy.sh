8a839891b7150760d4c0e3f780255f844bdd7b7e
cmong0516 <cmong0516@gmail.com>
2023-07-10 15:18:26 +0900

    fix(CI Settings): deploy.sh
    CI Settings
    - CiCd Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
