5b1e27c06b46435ab79f491a0ceae661efcd9b7d
cmong0516 <cmong0516@gmail.com>
2023-07-11 16:25:18 +0900

    fix(Application main Class Fix): Application main Class Fix
    Application main Class Fix
    - fix Application main Class Fix

--- src/main/java/com/hello/foreverpet/Application.java
+++ src/main/java/com/hello/foreverpet/Application.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
-public class ForeverpetApplication {
+public class Application {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(ForeverpetApplication.class, args);
-
-		SpringApplication application = new SpringApplication(ForeverpetApplication.class);
+		SpringApplication application = new SpringApplication(Application.class);
 		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
 	}
