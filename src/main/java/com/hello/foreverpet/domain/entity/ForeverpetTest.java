9a4c5f899f62eac492a599d51ea4ab3795426810
cmong0516 <cmong0516@gmail.com>
2023-07-11 16:42:05 +0900

    feat(RDS Make Entity Test): RDS Make Entity Test
    Feat RDS Make Entity Test
    - feat RDS Make Entity Test

---
+++ src/main/java/com/hello/foreverpet/domain/entity/ForeverpetTest.java
@@ -0,0 +1,10 @@
+package com.hello.foreverpet.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ForeverpetTest {
+    @Id
+    private Long id;
+}
